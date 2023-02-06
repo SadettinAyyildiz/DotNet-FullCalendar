bbb1f865347864cc3ba94c1f10de21b6d05826e6
Sadettin Ayyildiz <ayyildiz.s83@gmail.com>
2023-02-06 23:21:13 +0300

    AttributeRouting test

---
+++ DentistCalendar/DentistCalendar/Controllers/EventController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentistCalendar.Controllers
+{
+    [Route("event")]  // -> /event geldiğinde bu controller çalışır
+    public class EventController1 : Controller
+    {
+        [Route("")] //içi boş olduğu için default çalışır
+        [Route("~/add")] // önünde ne yazdığı önemli değil, add geldiğinde burası çalışır
+        public string Add() 
+        {
+            return "Add";
+        }
+
+        [Route("update/{id?}")] //opsiyonel olarak id gelebilir
+        public string Update(int id = 0)
+        {
+            return $"Update {id}";
+        }
+
+
+    }
+}
